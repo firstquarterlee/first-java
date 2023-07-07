2f389188998ddc0e24b1891841b4571596138f6b
firstquarterlee <firstquarterlee@gmail.com>
2023-07-07 22:07:07 +0900

    매개변수의 종류(기본형, 참조형)

---
+++ src/week03/parameter/Main.java
@@ -0,0 +1,23 @@
+package week03.parameter;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        // 기본형 매개변수
+        char type = 'D';
+        car.brakePedal(type);
+
+        System.out.println("type = " + type);
+        System.out.println("gear = " + car.gear);
+
+
+        System.out.println();
+        Tire tire = new Tire();
+        tire.company = "금호";
+
+        Tire carInstanceTire = car.setTire(tire);
+        System.out.println("tire.company = " + tire.company);
+        System.out.println("carInstanceTire.company = " + carInstanceTire.company);
+    }
+}
