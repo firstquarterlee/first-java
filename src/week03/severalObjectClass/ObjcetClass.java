3f6ff3f2222ef7d48b2b0663d0027c200797a181
firstquarterlee <firstquarterlee@gmail.com>
2023-07-11 23:24:58 +0900

    단일다중상속 final 최상위 클래스

---
+++ src/week03/severalObjectClass/ObjcetClass.java
@@ -0,0 +1,7 @@
+package week03.severalObjectClass;
+
+// Objcet clone() : 해당 객체의 복제본을 생성하여 반환함.
+// boolean equals(Object objcet) : 해당 객체와 전달받은 객체가 같은지 여부를 반환함.
+// Class getClass() : 해당 객체의 클래스 타입을 반환함.
+// int hashCode() : 자바에서 객체를 식별하는 정수값인 해시 코드를 반환함.
+// String toString() : 해당 객체의 정보를 문자열로 반환함. & Object 클래스에서는 클래스이름 @해쉬코드값 리터함.
